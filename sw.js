c0af2f2298401cf8379d60c08520947014f8520b
DK8178 <denniskern381@gmail.com>
2026-09-25 00:10:51 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='trading-journal-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>e.request.mode==='navigate'?caches.match('./index.html'):Promise.reject())))});
